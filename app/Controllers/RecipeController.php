08639bcaba6c95b53aaf9c4ca38f49a568e5929f
Anggit Pinto Yuwono <anggityuwono@gmail.com>
2023-02-04 23:05:51 +0700

    Trying entitiy

--- app/Controllers/RecipeController.php
+++ app/Controllers/RecipeController.php
@@ -3,13 +3,18 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use App\Models\Recipe;
+use App\Models\RecipeModel;
 
 class RecipeController extends BaseController
 {
     public function index()
     {
-        $recipe = model(Recipe::class);
-        echo var_dump($recipe->findAll());
+        $recipeModel = model(RecipeModel::class);
+        // echo var_dump($recipe->findAll());
+        $recipe = new \App\Entities\Recipe;
+        $recipe->recipe_name = 'Bakso Wonogiri';
+        $recipe->recipe_description = 'A comfort food. It is basically a meatball soup served with noodles and vermicelli';
+        $recipe->country_id = 1;
+        $recipeModel->save($recipe);
     }
 }
--- app/Models/RecipeModel.php
+++ app/Models/RecipeModel.php
@@ -3,21 +3,22 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use App\Entities\Recipe;
 
-class Recipe extends Model
+class RecipeModel extends Model
 {
     protected $DBGroup          = 'default';
     protected $table            = 'recipes';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
-    protected $returnType       = 'object';
+    protected $returnType       = Recipe::class;
     protected $useSoftDeletes   = true;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['recipe_name', 'recipe_description', 'country_id'];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
